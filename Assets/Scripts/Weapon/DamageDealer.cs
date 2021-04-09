631ce3ee0a562c541ebc58b1f809f545984344f3
Данил Панков <mistertimechip@mail.ru>
2021-04-09 16:05:01 +0500

    Added damage amount
    Added damage amount, actually calling an interface method to deal damage. Use functions instead of field get and set.

--- Assets/Scripts/Weapon/DamageDealer.cs
+++ Assets/Scripts/Weapon/DamageDealer.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    public bool canDealDamage { get; set; }
+    public bool canDealDamage => _canDealDamage;
+    private bool _canDealDamage;
+    private int _damageAmount;
     
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Damagable"))
-        {
-            if (canDealDamage)
-            {
-                Debug.Log("I did damage!");
-            }
-        }
+        if (!other.gameObject.CompareTag("Damageable")) return;
+        if (!canDealDamage) return;
+        var damageable = other.gameObject.GetComponent<IDamageable>();
+        damageable.Damage(_damageAmount);
+    }
+
+    public void Damage(int damageAmount)
+    {
+        _canDealDamage = true;
+        _damageAmount = damageAmount;
+    }
+
+    public void OnDamageEnd()
+    {
+        _canDealDamage = false;
     }
 }
