1094f0c46a58b91abaaaaf40d59adfa5a62e8d53
Данил Панков <mistertimechip@mail.ru>
2021-04-09 15:28:06 +0500

    Changes to interface
    Added damage and animation event

--- Assets/Scripts/Weapon/IWeapon.cs
+++ Assets/Scripts/Weapon/IWeapon.cs
@@ -1,6 +1,11 @@
+using UnityEngine;
+
 public interface IWeapon
 {
     void Attack();
     WeaponType WeaponType { get; }
     string WeaponName { get; }
+    float WeaponRadius { get; }
+    int WeaponDamage { get; }
+    void OnAttackEnded();
 }
